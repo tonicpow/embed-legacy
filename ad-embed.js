11db15a5f706a2d068f2a35eb2bf95b31242b56b
mrz1836 <mrz1818@gmail.com>
2019-05-04 09:17:29 -0400

    Refactor, working out the passing of variables

--- ad-embed.js
+++ ad-embed.js
@@ -1,27 +1,48 @@
 document.addEventListener("DOMContentLoaded", function(){
-  // Get sticker
-  // let stickerAddress = document.head.querySelector("[name=bitcoin-address][content]").content;
+  loadIframe();
+});
 
-  // Get query params
-  //let affiliate = getUrlParameter(affiliate)
+// loadIframe - loads the iframes in place of divs for ad-embed
+function loadIframe(){
+
+  // Get sticker
+  let stickerAddress = document.head.querySelector("[name=bitcoin-address][content]").content;
+
+  // Get query params from parent url
+  let affiliate = getUrlParameter("affiliate");
 
   // Get all tonic divs
   let ads = document.getElementsByClassName("tonic");
   if (ads.length === 0) {
-    console.log("no ad-divs found with class tonic");
+    console.error("no ad-divs found with class tonic");
+    return;
   }
 
-  for (let i = ads.length - 1; i >= 0; i--)
-  {
-    ads[i].setAttribute('data-iframe', 'found');
+  // Loop all ad divs that we found
+  for (let i = ads.length - 1; i >= 0; i--) {
 
+    // Get data-unit-id
+    let dataUnitId = ads[i].getAttribute('data-unit-id');
+    if (!dataUnitId || dataUnitId === "") {
+      console.error("missing data-unit-id");
+      return;
+    }
+
+    // Get data-pubkey
+    let dataPubKey = ads[i].getAttribute('data-pubkey');
+    if (!dataPubKey || dataPubKey === "") {
+      console.error("missing data-pubkey");
+      return;
+    }
+
+    // Build the iframe, pass along configuration variables
     let iframe = document.createElement('iframe');
-    iframe.src = "https://tonicpow.firebase.com";
+    iframe.src = "https://tonicpow.firebaseapp.com/?unit_id="+dataUnitId+"&pubkey="+dataPubKey+"&affiliate="+affiliate+"&sticker_address="+stickerAddress;
 
-
+    // Replace the div for the iframe
     ads[i].parentNode.replaceChild(iframe, ads[i]);
   }
-});
+}
 
 // Source: https://davidwalsh.name/query-string-javascript
 // getUrlParameter()
